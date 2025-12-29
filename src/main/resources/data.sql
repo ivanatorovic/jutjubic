a45f77120e8c0c20dd8742437438cb3932152bf9
ivanatorovic <ivana.torovic2003@gmail.com>
2025-12-29 17:53:29 +0100

    baza

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO video (title, description) VALUES
+                                           ('Video 1', 'Opis prvog videa'),
+                                           ('Video 2', 'Opis drugog videa'),
+                                           ('Spring Boot Test', 'Video ubačen preko data.sql');
